73721527988cc22363fa17969be4101587668772
Tony Li <litony396@gmail.com>
2026-04-24 23:49:21 -0700

    implemented uploadAudio which uploads a given audio file to Firebase Storage

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require('express')
-const { db, admin } = require('./firebase.js')
+const express = require('express');
+const { db, admin } = require('./firebase.js');
 
 const app = express();
---
+++ backend/services/storageService.js
@@ -0,0 +1,38 @@
+
+// import admin which was initialized earlier
+// the admin is used to perform operations on the firebase we set up
+const { admin } = require('../firebase.js');
+
+// load bucket used for audio storage from Firebase storage
+const BUCKET_NAME = 'plantcareai-f1498.firebasestorage.app';
+const bucket = admin.storage().bucket(BUCKET_NAME);
+
+// function that uploads audio to Firebase bucket
+const uploadAudio = async (audioData, audioName, username, fileType) => {
+    try {
+        // use a timestamp in order to make sure audio files are not rewritten
+        // for example, a user may upload two files with the same filename at different times -> we don't want the second file to overwrite the first one
+        // this fileName is where the audio is stored on the cloud storage
+        const timestamp = Date.now();
+        // Should follow guidelines on cloud storage naming from Google: https://docs.cloud.google.com/storage/docs/objects
+        // Used Gemini to generate this code to normalize the audio file name - replaces all weird characters with _
+        const safeName = audioName.replace(/[^a-zA-Z0-9.]/g, '_')
+        const fileName = `audio/${username}_${timestamp}_${safeName}`;
+
+        const file = bucket.file(fileName);
+
+        // upload the audio to the database
+        // also set the metadata so it is known that this is an audio file -> helps Gemini and if we need to playback audio
+        await file.save(audioData,{
+            metadata: {contentType: fileType}
+        });
+
+        // return URL to store in Firestore + give to Gemini to access
+        return `https://storage.googleapis.com/${bucket.name}/${fileName}`
+    } catch (error) {
+        console.error("Audio Upload Error:", error);
+        throw new Error("Audio Upload to Firebase Bucket Failed.")
+    }
+}
+
+module.exports = { uploadAudio };
