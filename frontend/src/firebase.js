d1776c8947d3a71da9b4d2a02629dc7c1d967ed0
Tony Li <litony396@gmail.com>
2026-04-27 23:40:10 -0700

    added firebase config for authentication

---
+++ frontend/src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+// https://firebase.google.com/docs/auth/web/google-signin
+// use firebase auth since it is really simple to use
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaGtd2RmAajfaPwPNR_zCt_09jMN6YLNE",
+    authDomain: "plantcareai-f1498.firebaseapp.com",
+    projectId: "plantcareai-f1498",
+    storageBucket: "plantcareai-f1498.firebasestorage.app",
+    messagingSenderId: "655658042217",
+    appId: "1:655658042217:web:2287d5b1b5ccfa82fe73f8",
+    measurementId: "G-JETJLRLH57"
+};
+
+// initialize firebase authorization
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+// Gemini said to write this line of code to allow permission to read user's Google Photos
+provider.addScope('https://www.googleapis.com/auth/photoslibrary.readonly');
+
+export { auth, provider, signInWithPopup };
