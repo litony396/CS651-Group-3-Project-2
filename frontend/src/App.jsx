ad386043f2d49df6509f80582aa1d9fd5646155e
Tony Li <litony396@gmail.com>
2026-04-29 05:21:15 -0700

    integrated routing between the dashboard and community page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import Dashboard from './pages/dashboard.jsx';
+import CommunityFeed from "./pages/communityFeed";
 import Login from "./pages/login";
+import './app.css';
 
 function App() {
     // used to store the user when logged in
     const [user, setUser] = React.useState(null);
 
+    // tracks which page is active (dashboard is default)
+    const [currentView, setCurrentView] = React.useState('dashboard');
+
     return (
-        <div className="App">
-            {/* basic routing to show dashboard if not logged in */}
-            {user ? (<Dashboard user={user}/>) : (<Login setUser={setUser} />)}
+        <div className="appContainer">
+            {/* basic routing to show main app if logged in, else login screen */}
+            {user ? (
+                <div>
+                    {/* switch between dashboard and community feed*/}
+                    <nav className="mainNav">
+                        <button
+                            onClick={() => setCurrentView('dashboard')}
+                            className={`navButton ${currentView === 'dashboard' ? 'active' : ''}`}
+                        >
+                            My Dashboard
+                        </button>
+
+                        <button
+                            onClick={() => setCurrentView('community')}
+                            className={`navButton ${currentView === 'community' ? 'active' : ''}`}
+                        >
+                            Community Feed
+                        </button>
+                    </nav>
+
+                    {/* choose which one to render */}
+                    {currentView === 'dashboard' ? (
+                        <Dashboard user={user} />
+                    ) : (
+                        <CommunityFeed />
+                    )}
+                </div>
+            ) : (
+                <Login setUser={setUser} />
+            )}
         </div>
     );
 }
