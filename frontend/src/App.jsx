c81bf1afc54b4a1b990144a5cd3027226aa6b238
Tony Li <litony396@gmail.com>
2026-04-29 17:24:34 -0700

    added code to log switching between web pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import ReactGA from "react-ga4";
 import Dashboard from './pages/dashboard.jsx';
 import CommunityFeed from "./pages/communityFeed";
 import Login from "./pages/login";
 import './app.css';
 
+ReactGA.initialize('G-4EX09RS1CW');
+
 function App() {
     // used to store the user when logged in
-    const [user, setUser] = React.useState(null);
+    const [user, setUser] = useState(null);
 
     // tracks which page is active (dashboard is default)
-    const [currentView, setCurrentView] = React.useState('dashboard');
+    const [currentView, setCurrentView] = useState('dashboard');
+
+    // log when user changes views
+    useEffect(() => {
+        ReactGA.send({
+            category: "Page View",
+            action: `Switched to ${currentView}`,
+            label: `PlantCareAI ${currentView} Screen`
+        });
+    }, [currentView]);
+
+    useEffect(() => {
+        // if the user variable becomes populated, they logged in
+        if (user) {
+            // tie this GA4 session to their specific UID
+            ReactGA.set({ userId: user.uid });
+
+            // log login event
+            ReactGA.event({
+                category: "Authentication",
+                action: "User Logged In",
+                label: "PlantCareAI Web Login"
+            });
+        }
+    }, [user]);
 
     return (
         <div className="appContainer">
